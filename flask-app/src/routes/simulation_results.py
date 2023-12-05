18844c3b0ac36424837a895c4131584491316f2f
amayam1 <152944264+amayam1@users.noreply.github.com>
2023-12-05 17:26:31 -0500

    Update simulation_results.py

--- flask-app/src/routes/simulation_results.py
+++ flask-app/src/routes/simulation_results.py
@@ -4,6 +4,7 @@ from src import db
 results_bp = Blueprint('results', __name__)
 
 @results_bp.route('/simulation_results', methods=['GET'])
+# getting all simulation results from a database
 def get_all_simulation_results():
     cursor = db.get_db().cursor()
     cursor.execute('SELECT * FROM simulation_results')
@@ -14,6 +15,7 @@ def get_all_simulation_results():
     return make_response(jsonify(json_data), 200)
 
 @results_bp.route('/simulation_results/<int:student_id>', methods=['GET'])
+# getting simulations result for specific student
 def get_results_for_student(student_id):
     cursor = db.get_db().cursor()
     cursor.execute('SELECT * FROM simulation_results WHERE student_id = ?', (student_id,))
@@ -24,6 +26,7 @@ def get_results_for_student(student_id):
     return make_response(jsonify(json_data), 200)
 
 @results_bp.route('/simulation_results/<int:student_id>/<int:simulation_id>', methods=['GET'])
+# getting a specific simulation result for a student
 def get_specific_result(student_id, simulation_id):
     cursor = db.get_db().cursor()
     cursor.execute('SELECT * FROM simulation_results WHERE student_id = ? AND simulation_id = ?', (student_id, simulation_id))
