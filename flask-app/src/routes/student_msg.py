fd742de0a3321b982afee6be5f4f22f337a01052
amayam1 <152944264+amayam1@users.noreply.github.com>
2023-12-05 17:28:40 -0500

    Update student_msg.py

--- flask-app/src/routes/student_msg.py
+++ flask-app/src/routes/student_msg.py
@@ -4,6 +4,7 @@ from src import db
 messages_bp = Blueprint('messages', __name__)
 
 @messages_bp.route('/student_messages/<int:student_id>', methods=['GET'])
+# get all messages for a specific student from the database
 def view_messages(student_id):
     cursor = db.get_db().cursor()
     cursor.execute('SELECT * FROM student_messages WHERE student_id = ?', (student_id,))
@@ -14,6 +15,7 @@ def view_messages(student_id):
     return make_response(jsonify(json_data), 200)
 
 @messages_bp.route('/student_messages/<int:student_id>', methods=['POST'])
+# Send a new message to a student
 def send_message(student_id):
     data = request.json
     current_app.logger.info(data)
