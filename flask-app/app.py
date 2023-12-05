6a26b9e0fb113b5e4c26a1c498c68c0a67510350
amayam1 <152944264+amayam1@users.noreply.github.com>
2023-12-05 17:19:15 -0500

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,17 +1,20 @@
-###
-# Main application interface
-###
+from flask import Flask
+from .simulations import simulations
+from .stocks import stocks
+from .resumes import resumes
+from .results import results
+from .internships import internships
+from .student_msg import student_msg
 
-# import the create app function 
-# that lives in src/__init__.py
-from src import create_app
+def create_app():
+    app = Flask(__name__)
 
-# create the app object
-app = create_app()
+    # Register Blueprints
+    app.register_blueprint(simulations, url_prefix='/simulations')
+    app.register_blueprint(stocks, url_prefix='/stocks')
+    app.register_blueprint(resumes, url_prefix='/resumes')
+    app.register_blueprint(simulation_results, url_prefix='/results')
+    app.register_blueprint(internships, url_prefix='/internships')
+    app.register_blueprint(student_msg, url_prefix='/communications')
 
-if __name__ == '__main__':
-    # we want to run in debug mode (for hot reloading) 
-    # this app will be bound to port 4000. 
-    # Take a look at the docker-compose.yml to see 
-    # what port this might be mapped to... 
-    app.run(debug = True, host = '0.0.0.0', port = 4000)
+    return app
