8526d96abcdac795c0343639dfbcb4e8633e803b
TimurEG <54975875+TimurEG@users.noreply.github.com>
2023-12-07 15:17:31 -0500

    final commit

--- flask-app/src/routes/internships.py
+++ flask-app/src/routes/internships.py
@@ -17,17 +17,18 @@ def list_internships():
     return make_response(jsonify(json_data), 200)
 
 
-@internships.route('/internships_available/<int:internship_id>', methods=['GET'])
-def get_specific_internship(internship_id):
+@internships.route('/internships_available/<company_id>', methods=['GET'])
+def get_specific_internship(company_id):
     # retrieve data for a specific internship
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM internship WHERE internship_id = ?', (internship_id,))
+    cursor.execute('SELECT * FROM internship WHERE company_id = ' + company_id)
     row_headers = [x[0] for x in cursor.description]
     row = cursor.fetchone()
-    if row:
-        return jsonify(dict(zip(row_headers, row)))
-    else:
-        return make_response(jsonify({'error': 'Internship not found'}), 404)
+    json_data = []
+    for row in cursor.fetchall():
+        json_data.append(dict(zip(row_headers, row)))
+    return jsonify(json_data)
+    
 
 
 @internships.route('/internships_available/company/<string:company_id>', methods=['DELETE'])
@@ -53,10 +54,9 @@ def update_specific_internship(internship_id):
 
 @internships.route('/internships_available', methods=['POST'])
 def create_internship():
-    # create a new internship
     internship_data = request.get_json()
     cursor = db.get_db().cursor()
-    query = 'INSERT INTO internship (name, description, url) VALUES (%s, %s, %s)'
-    cursor.execute(query, (internship_data['name'], internship_data['description'], internship_data['url']))
+    query = 'INSERT INTO internship (name, description, url, company_id) VALUES (%s, %s, %s, %s)'
+    cursor.execute(query, (internship_data['name'], internship_data['description'], internship_data['url'], internship_data['company_id']))
     db.get_db().commit()
     return jsonify({"message": "internship created successfully"}), 201
