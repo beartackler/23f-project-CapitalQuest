ae11caca92c64ad6bcd348eee74ddde32c778889
bancheta21 <ancheta.b@northeastern.edu>
2023-12-06 20:45:26 -0500

    adding stocks route

--- flask-app/src/routes/stocks.py
+++ flask-app/src/routes/stocks.py
@@ -1,72 +1,106 @@
-from flask import Blueprint, jsonify, request, current_app
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
 from src import db
 
 stocks = Blueprint('stocks', __name__)
 
-# GET /stocks: Get a list of all stocks
+
 @stocks.route('/stocks', methods=['GET'])
 def get_stocks():
+    # get a cursor object from the database
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT id, stock_name, quantity, price FROM stocks')
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT * FROM stocks')
+
+    # grab the column headers from the returned data
     column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
     json_data = []
 
-    the_data = cursor.fetchall()
-    for row in the_data:
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
 
-# POST /stocks: Create a new stock.
+
+@stocks.route('/stocks/<string:ticker>', methods=['GET'])
+def get_stock(ticker):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM stock WHERE ticker = %s', (ticker,))
+    column_headers = [x[0] for x in cursor.description]
+    row = cursor.fetchone()
+    if row:
+        return jsonify(dict(zip(column_headers, row)))
+    return jsonify({"message": "Stock not found"}), 404
+
+
+@stocks.route('/stocks/top5/bid', methods=['GET'])
+def get_top5_bid():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM stock ORDER BY bidPrice DESC LIMIT 5')
+    column_headers = [x[0] for x in cursor.description]
+    data = cursor.fetchall()
+    json_data = [dict(zip(column_headers, row)) for row in data]
+    return jsonify(json_data)
+
+
+@stocks.route('/stocks/top5/ask', methods=['GET'])
+def get_top5_ask():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM stock ORDER BY askPrice DESC LIMIT 5')
+    column_headers = [x[0] for x in cursor.description]
+    data = cursor.fetchall()
+    json_data = [dict(zip(column_headers, row)) for row in data]
+    return jsonify(json_data)
+
+
+@stocks.route('/stocks/top10/eps', methods=['GET'])
+def get_top10_eps():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM stock ORDER BY eps DESC LIMIT 10')
+    column_headers = [x[0] for x in cursor.description]
+    data = cursor.fetchall()
+    json_data = [dict(zip(column_headers, row)) for row in data]
+    return jsonify(json_data)
+
+
 @stocks.route('/stocks', methods=['POST'])
 def create_stock():
-    data = request.json
-    current_app.logger.info(data)
-
-    name = data['stock_name']
-    quantity = data['quantity']
-    price = data['price']
-
-    query = f'INSERT INTO stocks (stock_name, quantity, price) VALUES ("{name}", {quantity}, {price})'
-    current_app.logger.info(query)
-
+    data = request.get_json()
     cursor = db.get_db().cursor()
-    cursor.execute(query)
+    query = 'INSERT INTO stock (ticker, bidPrice, askPrice, eps, volume, beta) VALUES (%s, %s, %s, %s, %s, %s)'
+    cursor.execute(query, (data['ticker'], data['bidPrice'],
+                   data['askPrice'], data['eps'], data['volume'], data['beta']))
     db.get_db().commit()
+    return jsonify({"message": "Stock created successfully"}), 201
 
-    return jsonify({'status': 'Success', 'message': 'Stock created successfully'})
 
-# PUT /stocks/<stock_id>: Update values for a specific stock.
-@stocks.route('/stocks/<int:stock_id>', methods=['PUT'])
-def update_stock(stock_id):
-    data = request.json
-    current_app.logger.info(data)
-
-    name = data['stock_name']
-    quantity = data['quantity']
-    price = data['price']
-
-    query = f'''
-        UPDATE stocks
-        SET stock_name = "{name}", quantity = {quantity}, price = {price}
-        WHERE id = {stock_id}
-    '''
-    current_app.logger.info(query)
-
+@stocks.route('/stocks/<string:ticker>', methods=['PUT'])
+def update_stock(ticker):
+    data = request.get_json()
     cursor = db.get_db().cursor()
-    cursor.execute(query)
+    query = 'UPDATE stock SET bidPrice = %s, askPrice = %s, eps = %s, volume = %s, beta = %s WHERE ticker = %s'
+    cursor.execute(query, (data['bidPrice'], data['askPrice'],
+                   data['eps'], data['volume'], data['beta'], ticker))
     db.get_db().commit()
+    if cursor.rowcount == 0:
+        return jsonify({"message": "Stock not found"}), 404
+    return jsonify({"message": "Stock updated successfully"})
 
-    return jsonify({'status': 'Success', 'message': 'Stock updated successfully'})
 
-# DELETE /stocks/<stock_id>: Delete a specific stock.
-@stocks.route('/stocks/<int:stock_id>', methods=['DELETE'])
-def delete_stock(stock_id):
-    query = f'DELETE FROM stocks WHERE id = {stock_id}'
-    current_app.logger.info(query)
-
+@stocks.route('/stocks/<string:ticker>', methods=['DELETE'])
+def delete_stock(ticker):
     cursor = db.get_db().cursor()
-    cursor.execute(query)
+    cursor.execute('DELETE FROM stock WHERE ticker = %s', (ticker,))
     db.get_db().commit()
-
-    return jsonify({'status': 'Success', 'message': 'Stock deleted successfully'})
+    if cursor.rowcount == 0:
+        return jsonify({"message": "Stock not found"}), 404
+    return jsonify({"message": "Stock deleted successfully"})
